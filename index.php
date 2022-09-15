e512cf87ba6d888ba955ae83086f04999ba7a4a3
XPNew-472 <103023451+XPNew-472@users.noreply.github.com>
2022-09-15 13:08:45 +0200

    Update index.php
    reflects documentation's tab name

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Generic Blog (beta) - Main Page</title>
+    <title>Generic Blog Beta - Main Page</title>
 </head>
 <body>
     <h1>You landed in the beta of a Generic Blog. Leave me alone.</h1>
