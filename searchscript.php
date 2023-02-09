ede44b83282368b70fe5d2eb2005b98cf01dd7a7
XPCurrent <xpnew472@outlook.com>
2023-02-09 14:52:39 +0100

    removed reference to unseenstore that broke stuff

--- searchscript.php
+++ searchscript.php
@@ -1,7 +1,6 @@
 <?php
 require_once "connect.php";
 $connection = new mysqli($host,$user,$pass,$dbname);
-	mysqli_connect("localhost", "root", "", "unseenstorenumber2") or die("Error connecting to database: ".mysqli_error());
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
